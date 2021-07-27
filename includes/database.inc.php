6dab7a86d000c18641116fd7dcc00723cbb5f1f0
wealthystudent <86958682+wealthystudent@users.noreply.github.com>
2021-07-27 21:59:44 +0200

    Add files via upload

---
+++ includes/database.inc.php
@@ -0,0 +1,9 @@
+<?php
+	$dbServerName = "test.dev";
+	$dbUsername = "teknostart_bruker";
+	$dbPassword = "abcde12345";
+	//Name of the database
+	$dbName = "testing_database";
+
+	$connection = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
+?>
