3d09626d76538c268fdf1b9876b70ed5d4c467b1
wealthystudent <86958682+wealthystudent@users.noreply.github.com>
2021-07-30 19:26:54 +0200

    Delete includes directory

--- includes/database.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$dbServerName = "test.dev";
-	$dbUsername = "teknostart_bruker";
-	$dbPassword = "abcde12345";
-	//Name of the database
-	$dbName = "testing_database";
-
-	$connection = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
-?>
