a89433c636fc0202d78563bd79750a0b85e9dd48
Kristian Astad Dupont <Kristian@astadupont.dk>
2023-08-07 15:48:00 +0200

    Forking and updating some ip addresses

--- connection.php
+++ connection.php
@@ -1,13 +1,12 @@
 <?php	
 	//This one includes the database connection
 	//include_once dirname(__FILE__) . '/../includes/database.inc.php';
-	$dbServerName = "test.dev";
-	$dbUsername = "teknostart_bruker";
-	$dbPassword = "abcde12345";
-	$dbName = "teknostart_login";
+	$dbServerName = "localhost";
+	$dbUsername = "admin";
+	$dbPassword = "Teknostart";
+	$dbName = "ELDIts";
 	
 	if(!$connection = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName)){
 		die("Failed to connect!");
 	}
-	
 ?>
